7412ca88ad1ac60dc5fafd2cf76a3bc3afd80b9b
Matt <zilleym@uni.edu>
2020-08-04 13:18:37 -0500

    hero section, main menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "375px";
+    document.getElementById("main").style.marginLeft = "375px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
