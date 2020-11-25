f52fd27b5bf3c86fcfb4fa2be5c044121c38f92a
Matt <matt.zilley@redlabtech.com>
2020-11-24 19:48:40 -0600

    post-live edits

--- js/loadTemplate.js
+++ js/loadTemplate.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
   const templates = ["footer", "navbar", "contactus", "preloader"];
 
   templates.forEach((template) => {
-    $(`#${template}`).load(`templates/${template}.html`);
+    $(`#${template}`).load(`templates/${template}`);
   });
 });
--- js/script.js
+++ js/script.js
@@ -108,8 +108,6 @@ jQuery(document).ready(function($) {
         }
         ]
     });
-
-    $("#phone").inputmask({"mask": "(999) 999-9999"});
 });
 
 
