159e6703817800b6376a49bf2ffc41d321e1a9f6
Matt <matt.zilley@redlabtech.com>
2021-02-06 10:48:51 -0600

    using staging site for once - smart

--- js/script.js
+++ js/script.js
@@ -111,82 +111,73 @@ jQuery(document).ready(function($) {
 });
 
 
-// Responsive Image Comparison Slider https://codepen.io/bamf/pen/jEpxOX
-$('.ba-slider').each(function(){
-  var cur = $(this);
-  // Adjust the slider
-  var width = cur.width()+'px';
-  cur.find('.resize img').css('width', width);
-  // Bind dragging events
-  drags(cur.find('.handle'), cur.find('.resize'), cur);
-});
 
 // Update sliders on resize. 
 // Because we all do this: i.imgur.com/YkbaV.gif
-$(window).resize(function(){
-  $('.ba-slider').each(function(){
-    var cur = $(this);
-    var width = cur.width()+'px';
-    cur.find('.resize img').css('width', width);
-  });
-});
+// $(window).resize(function(){
+//   $('.ba-slider').each(function(){
+//     var cur = $(this);
+//     var width = cur.width()+'px';
+//     cur.find('.resize img').css('width', width);
+//   });
+// });
 
-function drags(dragElement, resizeElement, container) {
+// function drags(dragElement, resizeElement, container) {
 	
-  // Initialize the dragging event on mousedown.
-  dragElement.on('mousedown touchstart', function(e) {
+//   // Initialize the dragging event on mousedown.
+//   dragElement.on('mousedown touchstart', function(e) {
     
-    dragElement.addClass('draggable');
-    resizeElement.addClass('resizable');
+//     dragElement.addClass('draggable');
+//     resizeElement.addClass('resizable');
     
-    // Check if it's a mouse or touch event and pass along the correct value
-    var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
+//     // Check if it's a mouse or touch event and pass along the correct value
+//     var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
     
-    // Get the initial position
-    var dragWidth = dragElement.outerWidth(),
-        posX = dragElement.offset().left + dragWidth - startX,
-        containerOffset = container.offset().left,
-        containerWidth = container.outerWidth();
+//     // Get the initial position
+//     var dragWidth = dragElement.outerWidth(),
+//         posX = dragElement.offset().left + dragWidth - startX,
+//         containerOffset = container.offset().left,
+//         containerWidth = container.outerWidth();
  
-    // Set limits
-    minLeft = containerOffset + 10;
-    maxLeft = containerOffset + containerWidth - dragWidth - 10;
+//     // Set limits
+//     minLeft = containerOffset + 10;
+//     maxLeft = containerOffset + containerWidth - dragWidth - 10;
     
-    // Calculate the dragging distance on mousemove.
-    dragElement.parents().on("mousemove touchmove", function(e) {
+//     // Calculate the dragging distance on mousemove.
+//     dragElement.parents().on("mousemove touchmove", function(e) {
     	
-      // Check if it's a mouse or touch event and pass along the correct value
-      var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
+//       // Check if it's a mouse or touch event and pass along the correct value
+//       var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
       
-      leftValue = moveX + posX - dragWidth;
+//       leftValue = moveX + posX - dragWidth;
       
-      // Prevent going off limits
-      if ( leftValue < minLeft) {
-        leftValue = minLeft;
-      } else if (leftValue > maxLeft) {
-        leftValue = maxLeft;
-      }
+//       // Prevent going off limits
+//       if ( leftValue < minLeft) {
+//         leftValue = minLeft;
+//       } else if (leftValue > maxLeft) {
+//         leftValue = maxLeft;
+//       }
       
-      // Translate the handle's left value to masked divs width.
-      widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+//       // Translate the handle's left value to masked divs width.
+//       widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
 			
-      // Set the new values for the slider and the handle. 
-      // Bind mouseup events to stop dragging.
-      $('.draggable').css('left', widthValue).on('mouseup touchend touchcancel', function () {
-        $(this).removeClass('draggable');
-        resizeElement.removeClass('resizable');
-      });
-      $('.resizable').css('width', widthValue);
-    }).on('mouseup touchend touchcancel', function(){
-      dragElement.removeClass('draggable');
-      resizeElement.removeClass('resizable');
-    });
-    e.preventDefault();
-  }).on('mouseup touchend touchcancel', function(e){
-    dragElement.removeClass('draggable');
-    resizeElement.removeClass('resizable');
-  });
-}
+//       // Set the new values for the slider and the handle. 
+//       // Bind mouseup events to stop dragging.
+//       $('.draggable').css('left', widthValue).on('mouseup touchend touchcancel', function () {
+//         $(this).removeClass('draggable');
+//         resizeElement.removeClass('resizable');
+//       });
+//       $('.resizable').css('width', widthValue);
+//     }).on('mouseup touchend touchcancel', function(){
+//       dragElement.removeClass('draggable');
+//       resizeElement.removeClass('resizable');
+//     });
+//     e.preventDefault();
+//   }).on('mouseup touchend touchcancel', function(e){
+//     dragElement.removeClass('draggable');
+//     resizeElement.removeClass('resizable');
+//   });
+// }
 
 /*!
  * jQuery Accordion 0.0.1
