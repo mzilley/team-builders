617851d40d57a0f16d088d4feb9283f925b7bca3
Matt <matt.zilley@redlabtech.com>
2021-05-18 20:29:33 -0500

    catch staging site up

--- js/script.js
+++ js/script.js
@@ -42,14 +42,14 @@ jQuery(document).ready(function($) {
       });
 
       // Calendly iFrame
-      $('.calendly').hide();
-      $('.calendly-scheduler').on('click', function() {
-        $('.calendly').show("3250");
-      });
+    //   $('.calendly').hide();
+    //   $('.calendly-scheduler').on('click', function() {
+    //     $('.calendly').show("3250");
+    //   });
 
-      $('#calendly-scheduler-close').on('click', function() {
-        $('.calendly').hide("3250");
-      });
+    //   $('#calendly-scheduler-close').on('click', function() {
+    //     $('.calendly').hide("3250");
+    //   });
 
       // Menu Toggle
       $('#main-menu').hide();
@@ -62,16 +62,17 @@ jQuery(document).ready(function($) {
       });
 
       // Hero Slick Slider
-      $('.fade').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 1000,
-        fade: false,
-        autoplay: true,
-        cssEase: 'linear'
-      });
+    //   $('.fade').slick({
+    //     dots: false,
+    //     arrows: false,
+    //     infinite: true,
+    //     speed: 1000,
+    //     fade: false,
+    //     autoplay: true,
+    //     cssEase: 'linear'
+    //   });
 
+    // Testimonials Slider
       $('.testimonials').slick({
       dots: false,
       arrows: true,
@@ -102,88 +103,15 @@ jQuery(document).ready(function($) {
         {
           breakpoint: 768,
           settings: {
-            slidesToShow: 1,
+              slidesToShow: 1,
+              arrows: false,
+              dots: true,
             slidesToScroll: 1
           }
         }
         ]
     });
 });
-
-
-
-// Update sliders on resize. 
-// Because we all do this: i.imgur.com/YkbaV.gif
-// $(window).resize(function(){
-//   $('.ba-slider').each(function(){
-//     var cur = $(this);
-//     var width = cur.width()+'px';
-//     cur.find('.resize img').css('width', width);
-//   });
-// });
-
-// function drags(dragElement, resizeElement, container) {
-	
-//   // Initialize the dragging event on mousedown.
-//   dragElement.on('mousedown touchstart', function(e) {
-    
-//     dragElement.addClass('draggable');
-//     resizeElement.addClass('resizable');
-    
-//     // Check if it's a mouse or touch event and pass along the correct value
-//     var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
-    
-//     // Get the initial position
-//     var dragWidth = dragElement.outerWidth(),
-//         posX = dragElement.offset().left + dragWidth - startX,
-//         containerOffset = container.offset().left,
-//         containerWidth = container.outerWidth();
- 
-//     // Set limits
-//     minLeft = containerOffset + 10;
-//     maxLeft = containerOffset + containerWidth - dragWidth - 10;
-    
-//     // Calculate the dragging distance on mousemove.
-//     dragElement.parents().on("mousemove touchmove", function(e) {
-    	
-//       // Check if it's a mouse or touch event and pass along the correct value
-//       var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
-      
-//       leftValue = moveX + posX - dragWidth;
-      
-//       // Prevent going off limits
-//       if ( leftValue < minLeft) {
-//         leftValue = minLeft;
-//       } else if (leftValue > maxLeft) {
-//         leftValue = maxLeft;
-//       }
-      
-//       // Translate the handle's left value to masked divs width.
-//       widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
-			
-//       // Set the new values for the slider and the handle. 
-//       // Bind mouseup events to stop dragging.
-//       $('.draggable').css('left', widthValue).on('mouseup touchend touchcancel', function () {
-//         $(this).removeClass('draggable');
-//         resizeElement.removeClass('resizable');
-//       });
-//       $('.resizable').css('width', widthValue);
-//     }).on('mouseup touchend touchcancel', function(){
-//       dragElement.removeClass('draggable');
-//       resizeElement.removeClass('resizable');
-//     });
-//     e.preventDefault();
-//   }).on('mouseup touchend touchcancel', function(e){
-//     dragElement.removeClass('draggable');
-//     resizeElement.removeClass('resizable');
-//   });
-// }
-
-/*!
- * jQuery Accordion 0.0.1
- * (c) 2014 Victor Fernandez <victor@vctrfrnndz.com>
- * MIT Licensed.
- */
 
 ;(function ( $, window, document, undefined ) {
 
