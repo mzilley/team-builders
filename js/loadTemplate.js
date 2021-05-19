a6120bea290addde3aa8f4a6682850edee4dccb8
Matt <matt.zilley@redlabtech.com>
2021-05-19 14:45:57 -0500

    create footer partial

--- js/loadTemplate.js
+++ js/loadTemplate.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  const templates = ["footer", "navbar", "contactus", "preloader", "footer-scripts"];
+  const templates = ["footer", "navbar", "contactus", "footer-scripts"];
 
   templates.forEach((template) => {
     $(`#${template}`).load(`templates/${template}`);
